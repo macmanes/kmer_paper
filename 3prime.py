0ea4a66c450b36f149d2c38617d4c6f4511f4ad3
Matt MacManes <macmanes@gmail.com>
2014-01-27 07:13:02 -0500

    clean up comments

--- 3prime.py
+++ 3prime.py
@@ -5,8 +5,4 @@ import os
 
 
 for seq in SeqIO.parse(open(sys.argv[1], "rU"), "fasta"):
-#	print "%s%s%s%s" % ('>',seq.id, '\n', seq.seq[0:24])
-#	print "%s%s%s%s" % ('>',seq.id, '\n', seq.seq[25:49])
-#	print "%s%s%s%s" % ('>',seq.id, '\n', seq.seq[50:74])
 	print "%s%s%s%s" % ('>',seq.id, '\n', seq.seq[-25:])
-#        print "%s%s%s%s" % ('>',seq.id, '\n', seq.seq[:-25])
